7f3567723cfc118892c15dfe19b0dbe98beea7d0
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-06-04 21:25:44 -0500

    Added handleClick, handleIncrement and handleReset to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import './App.css';
-import Nav from './components/Nav';
-import friends from "./friends.json";
 import FriendCard from "./components/FriendCard";
+import Nav from './components/Nav';
+
+import friends from "./friends.json";
+import './App.css';
 
 function shuffleFriends(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -19,6 +20,39 @@ class App extends Component {
     topScore: 0,
     rightWrong: "",
     clicked: []
+  };
+
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.setState.clicked.concat(id) })
+    } else {
+      this.handleReset();
+    }
+  };
+
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      rightWrong: ""
+    });
+    if (newScore > this.state.topScore) {
+      this.setState({ topScore: newScore });
+    } else if (newScore === 12) {
+      this.setState({ rightWrong: "You Win!" })
+    }
+    this.handleShuffle();
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      rightWrong: "Giggity",
+      clicked: []
+    });
+    this.handleShuffle();
   };
   
   handleShuffle = () => {
@@ -52,6 +86,9 @@ class App extends Component {
           <div className="col-md-3 col-sm-6">
             <FriendCard
               key={friend.id}
+              handleClick={this.handleClick}
+              handleIncrement={this.handleIncrement}
+              handleReset={this.handleReset}
               handleShuffle={this.handleShuffle}
               id={friend.id}
               image={friend.image}
