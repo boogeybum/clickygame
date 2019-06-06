af44b5d23caf101d093d3d6c1d624ef8f1b0a7d4
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-06-05 21:27:48 -0500

    Updates

---
+++ precache-manifest.068b0ab43674f49a4b1056a8de9cc905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31a1b9890b16a4039a5a9a08893d87a8",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "b18d850a5fbf6c6e0b31",
+    "url": "/clickygame/static/css/main.59ca3b68.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "b18d850a5fbf6c6e0b31",
+    "url": "/clickygame/static/js/main.df7d2f1b.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
